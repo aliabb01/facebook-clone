90258468baf1d224fb534eab2383bc30d6b2bfcb
aliabb01 <aliabbasov21112001@gmail.com>
2021-05-13 22:53:41 +0400

    StoryReel component

---
+++ src/StoryReel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './StoryReel.css';
+import Story from './Story';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+                title="Ali Abbasov" /> 
+           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+                title="Ali Abbasov" /> 
+           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+                title="Ali Abbasov" /> 
+           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+                title="Ali Abbasov" /> 
+           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+                title="Ali Abbasov" /> 
+        </div>
+    )
+}
+
+export default StoryReel
