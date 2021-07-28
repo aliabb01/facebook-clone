c5e01166baf04eed2ffed0682e66f688646e9ba9
aliabb <ali3abbasov@gmail.com>
2021-07-28 21:05:48 +0400

    refactor: Updated StoryReel data, correct width

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -5,16 +5,16 @@ import Story from './Story';
 function StoryReel() {
     return (
         <div className="storyReel">
-           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
+           <Story image="https://i.pinimg.com/originals/47/6c/2e/476c2e56a7d91def1054f5b7770cb24d.jpg"
                 title="Ali Abbasov" /> 
+           <Story image="https://i.pinimg.com/originals/30/93/cb/3093cb01d9de2d125fda8ba5e3e41946.jpg"
+                title="John Doe" /> 
            <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
-                title="Ali Abbasov" /> 
-           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
-                title="Ali Abbasov" /> 
-           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
-                title="Ali Abbasov" /> 
-           <Story image="https://i.pinimg.com/originals/83/64/66/83646654668bf9ae412f45bb2e417ddf.jpg"
-                title="Ali Abbasov" /> 
+                title="Max Wagner" /> 
+           <Story image="https://i.pinimg.com/originals/9c/57/69/9c5769f09d26de7b31ee6012098b5dcd.jpg"
+                title="Kaitlyn Mcgee" /> 
+           <Story image="https://i.pinimg.com/originals/79/b2/f0/79b2f08b70ac3a6d6c59c7a04ecf013d.jpg"
+                title="Jack Parker" /> 
         </div>
     )
 }
